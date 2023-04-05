94116c77f829390ceed5a592c6d2ecf9efbd6c6e
Shivani Amborkar <shivaniamborkar6@gmail.com>
2023-04-05 23:34:26 +0530

    added dyanmic job page for each job

--- app.py
+++ app.py
@@ -39,7 +39,9 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
-    return jsonify(job)
+    if not job:
+        return "Not found", 404
+    return render_template('jobpage.html', job=job)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
